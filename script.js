c3fd9be5c045b8e7dc686d2978e7ba8e9dcf4c4d
BitHero <nuhumusah874@gmail.com>
2023-05-31 15:13:32 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const repo = document.getElementById('repo')
+const repoBtn = document.getElementById('repoBtn')
+
+repoBtn.addEventListener('click', generateRepo)
+generateRepo()
+
+function generateRepo() {
+    fetch('https://api.github.com/search/repositories?q=language:python&sort=stars', {
+        headers : {'Accept': 'application/vnd.github.v3+json'}
+    }).then(response => response.json()).then(data => {
+        let repoEl = data['items']
+        for(let element of repoEl) {
+            repo.innerHTML = element['owner']['url']
+        } 
+    })
+}
